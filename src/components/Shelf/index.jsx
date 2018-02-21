5378499b62f5621d79ce9e8eecdcb65f77e7a4e3
Abishek Aditya <abishekaditya@outlook.com>
2018-02-21 13:50:51 +0530

    added shelf title

--- src/components/Shelf/index.jsx
+++ src/components/Shelf/index.jsx
@@ -8,7 +8,7 @@ const Shelf = ({ data, text }) => {
   const books = data.map(book => <Book data={book} />);
   return (
     <div className="Shelf-container">
-      <p>{text}</p>
+      <div className="Shelf-title">{text}</div>
       <div className="Book-container">
         {books}
       </div>
